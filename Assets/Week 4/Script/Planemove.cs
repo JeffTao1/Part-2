ff6b3229737e927846ee7d6f55640641fdf20291
taoje <taoje@OA-S141-27>
2024-02-01 10:28:00 -0500

    task2 compele

--- Assets/Week 4/Script/Planemove.cs
+++ Assets/Week 4/Script/Planemove.cs
@@ -8,11 +8,48 @@ public class Planemove : MonoBehaviour
     public float newPositionTHreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer line;
+    Rigidbody2D rb;
+    Vector2 currentPosition;
+    public float speed = 1;
     private void Start()
     {
         line = GetComponent<LineRenderer>();
         line.positionCount = 1;
         line.SetPosition(0, transform.position);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0) {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
+            rb.rotation =  -angle;
+
+
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        line.SetPosition(0, transform.position);
+        if (points.Count>0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPositionTHreshold) 
+            { 
+            
+                points.RemoveAt(0);
+                for (int i = 0; i < line.positionCount - 2; i++) 
+                {
+                    line.SetPosition(i, line.GetPosition(i + 1));
+                
+                }
+                line.positionCount--;
+            
+            }
+        }
     }
     private void OnMouseDown()
     {
