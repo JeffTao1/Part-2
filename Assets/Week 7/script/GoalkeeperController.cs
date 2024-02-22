c12fdf799065ea0dafd40931947292a6aff74a4c
taoje <jefft18ca@gmail.com>
2024-02-22 12:55:47 -0500

    part of task4

---
+++ Assets/Week 7/script/GoalkeeperController.cs
@@ -0,0 +1,21 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
